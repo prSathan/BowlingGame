7df0f2c09b99fb9583028d72b10233ebc124d2e2
Prabu Sathan <prabu.sathan@gmail.com>
2017-06-27 10:02:09 +0100

    Able to create a game and roll a gutter game

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,15 @@
+describe('The Bowling Game', function () {
+
+  it('can create game', function () {
+    var game = new BowlingGame();
+  });
+
+  it('can roll gutter game', function () {
+    var game = new BowlingGame();
+    for (var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toBe(0);
+  });
+  
+});
---
+++ src/BowlingGame.js
@@ -0,0 +1,11 @@
+function BowlingGame() {
+
+}
+
+BowlingGame.prototype.roll = function (pins) {
+
+};
+
+BowlingGame.prototype.score = function () {
+  return 0
+};
