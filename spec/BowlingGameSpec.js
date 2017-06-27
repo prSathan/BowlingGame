edc177e921ef773cd0906d9f9f91972179ddfd77
Prabu Sathan <prabu.sathan@gmail.com>
2017-06-27 10:36:34 +0100

    it can roll a score

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -11,5 +11,13 @@ describe('The Bowling Game', function () {
     }
     expect(game.score()).toBe(0);
   });
-  
+
+  it('can roll all ones', function () {
+    var game = new BowlingGame();
+    for (var i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toBe(20);
+  })
+
 });
